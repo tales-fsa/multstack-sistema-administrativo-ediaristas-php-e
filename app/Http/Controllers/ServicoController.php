9a364f2a3007cb414bfc3a5f18754d54848d5cc6
tales-fsa <tales.fsa@hotmail.com>
2021-12-01 02:28:08 -0300

    Mensagens de feedbak e ajustes de validação

--- app/Http/Controllers/ServicoController.php
+++ app/Http/Controllers/ServicoController.php
@@ -6,7 +6,11 @@ use App\Http\Requests\ServicoRequest;
 use App\Models\Servico;
 
 class ServicoController extends Controller
-{
+{   /**
+    * Lista os serviços
+    *
+    * @return \Illuminate\Contracts\View\View|\Illuminate\Contracts\View\Factory
+    */
     public function index()
     {
         $servicos = Servico::simplepaginate(10);
@@ -14,20 +18,37 @@ class ServicoController extends Controller
         return view('servicos.index')->with('servicos', $servicos);
     }
 
+    /**
+     * Mostra o form vazio para criação
+     *
+     * @return \Illuminate\Contracts\View\View|\Illuminate\Contracts\View\Factory
+     */
     public function create()
     {
         return view('servicos.create');
     }
 
+    /**
+     * Cria um novo registro no banco de dados
+     *
+     * @param ServicoRequest $request
+     * @return \Illuminate\Routing\Redirector|\Illuminate\Http\RedirectResponse
+     */
     public function store(ServicoRequest $request)
     {
         $dados = $request->except('_token');
 
         Servico::create($dados);
 
-        return redirect()->route('servicos.index');
+        return redirect()->route('servicos.index')->with('mensagem', 'Serviço criado com sucesso!');
     }
 
+    /**
+     * Mostra o formulário preenchido para alteração
+     *
+     * @param integer $id
+     * @return \Illuminate\Contracts\View\View|\Illuminate\Contracts\View\Factory
+     */
     public function edit(int $id)
     {
         $servico = Servico::findOrFail($id);
@@ -35,12 +56,19 @@ class ServicoController extends Controller
         return view('servicos.edit')->with('servico', $servico);
     }
 
+    /**
+     * Atualiza um registro no banco de dados
+     *
+     * @param integer $id
+     * @param ServicoRequest $request
+     * @return \Illuminate\Routing\Redirector|\Illuminate\Http\RedirectResponse
+     */
     public function update(int $id, ServicoRequest $request)
     {
         $dados = $request->except(['_token', '_method']);
         $servico = Servico::findOrFail($id);
         $servico->update($dados);
 
-        return redirect()->route('servicos.index');
+        return redirect()->route('servicos.index')->with('mensagem', 'Serviço editado com sucesso!');;
     }
 }
--- resources/views/servicos/_form.blade.php
+++ resources/views/servicos/_form.blade.php
@@ -15,9 +15,9 @@
                         <label for="icone">Ícone</label>
                         <select name="icone" id="icone" class="form-control">
                             <option value="">Selecione o ícone</option>
-                            <option value="twf-cleaning-1" {{old('icone', isset($servico) && $servico->icone) === 'twf-cleposicaoaning-1' ? 'selected' : '' }}>ícone 1</option>
-                            <option value="twf-cleaning-2" {{old('icone', isset($servico) && $servico->icone) === 'twf-cleaning-2' ? 'selected' : '' }}>ícone 2</option>
-                            <option value="twf-cleaning-3" {{old('icone', isset($servico) && $servico->icone) === 'twf-cleaning-3' ? 'selected' : '' }}>ícone 3</option>
+                            <option value="twf-cleaning-1" {{ old('icone', $servico->icone ?? '') === 'twf-cleaning-1' ? 'selected' : '' }}>ícone 1</option>
+                            <option value="twf-cleaning-2" {{ old('icone', $servico->icone ?? '') === 'twf-cleaning-2' ? 'selected' : '' }}>ícone 2</option>
+                            <option value="twf-cleaning-3" {{ old('icone', $servico->icone ?? '') === 'twf-cleaning-3' ? 'selected' : '' }}>ícone 3</option>
                         </select>
                     </div>
                 </div>
